112599327db76a234080e14860b9315dbed325fe
Diego Rodríguez <diego.rodriguez@rootstrap.com>
2023-04-27 15:16:56 -0300

    Remove unnecessary fragment

--- src/app/(not-authenticated)/sign_up/page.tsx
+++ src/app/(not-authenticated)/sign_up/page.tsx
@@ -18,42 +18,40 @@ export default function SignUp() {
   };
 
   return (
-    <>
-      <section className="mb-5 ml-30 mt-16 flex h-139 w-89 flex-col items-center justify-between rounded-lg bg-white px-9 py-8">
-        <h1 className="mt-4">
-          <Image src={logo} alt="Black Market" />
-        </h1>
-        <Input
-          label="Email"
-          name="email"
-          type="text"
-          placeholder="Type your email"
-          handlechange={setEmail}
-        />
-        <Input
-          label="Full Name"
-          name="name"
-          type="name"
-          placeholder="Type your full name"
-          handlechange={setName}
-        />
-        <Input
-          label="Password"
-          name="password"
-          type="password"
-          placeholder="Type your password"
-          hideButton={true}
-          handlechange={setPassword}
-        />
-        <Button text="Sign up" handleClick={myButtonFunction} />
-        <div className="text-center">
-          By signing up, you accept the <BMLink url="" text="Data Policy" /> and
-          the <BMLink url="" text="Cookies Policy." />
-        </div>
-        <div>
-          Already have an account? <BMLink url="/sign_in" text="Log in" />
-        </div>
-      </section>
-    </>
+    <section className="mb-5 ml-30 mt-16 flex h-139 w-89 flex-col items-center justify-between rounded-lg bg-white px-9 py-8">
+      <h1 className="mt-4">
+        <Image src={logo} alt="Black Market" />
+      </h1>
+      <Input
+        label="Email"
+        name="email"
+        type="text"
+        placeholder="Type your email"
+        handlechange={setEmail}
+      />
+      <Input
+        label="Full Name"
+        name="name"
+        type="name"
+        placeholder="Type your full name"
+        handlechange={setName}
+      />
+      <Input
+        label="Password"
+        name="password"
+        type="password"
+        placeholder="Type your password"
+        hideButton={true}
+        handlechange={setPassword}
+      />
+      <Button text="Sign up" handleClick={myButtonFunction} />
+      <div className="text-center">
+        By signing up, you accept the <BMLink url="" text="Data Policy" /> and
+        the <BMLink url="" text="Cookies Policy." />
+      </div>
+      <div>
+        Already have an account? <BMLink url="/sign_in" text="Log in" />
+      </div>
+    </section>
   );
 }
