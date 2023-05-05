a974d99c993cec7627756e294873127f70213bf3
Diego Rodríguez <diego.rodriguez@rootstrap.com>
2023-05-05 12:18:47 -0300

    Remove some unneeded stuff

--- next.config.js
+++ next.config.js
@@ -3,14 +3,6 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:4200/:path*',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,3 @@
-const BASE_URL = ENV['base_url'];
-
 export async function GET(request: Request) {
   return new Response('Hello, Next.js!');
 }
